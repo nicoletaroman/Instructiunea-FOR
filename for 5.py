5a5205371513d2f4fa6c424247ea26e9021b4af2
nicoletaroman <70715983+nicoletaroman@users.noreply.github.com>
2020-11-07 18:26:23 +0200

    Add files via upload

--- for 5.py
+++ for 5.py
@@ -1,6 +1,6 @@
+n=int(input("dati un numar:"))
 s=0
-n=eval(input("dati un nr"))
-for nr in range(1,n+1):
-    if nr%5==0 and nr%3==0:
-        s=nr+s
-        print("suma este",s)
+for i in range(1,n+1):
+    if (i%3==0) and (i%5==0):
+        s+=i
+print("suma este =",s)
